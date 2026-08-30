507f4b94eb1babd6a2f4ae0670017ddb1fe015ed
Kawsar37 <kawsarali750@gmail.com>
2026-08-30 20:01:09 +0600

    refactor(import): standardize module path aliases in import marks page

--- frontend/src/app/import/page.tsx
+++ frontend/src/app/import/page.tsx
@@ -2,9 +2,9 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { Header } from '../../components/Header';
-import { api } from '../../lib/api';
-import { useCase } from '../../context/CaseContext';
+import { Header } from '@/components/Header';
+import { api } from '@/lib/api';
+import { useCase } from '@/context/CaseContext';
 
 export default function ImportMarksPage() {
   const { activeCase } = useCase();
